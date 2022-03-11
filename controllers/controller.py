e64daa78c79bcc6245360c8d8eefb6c1321b67c6
CordiiT14 <cordelia.toennies@live.com>
2022-03-11 10:55:31 +0000

    App modules and packages set up complete

---
+++ controllers/controller.py
@@ -0,0 +1,17 @@
+from flask import render_template, request
+from app import app
+from models.player import Player
+from models.game import Game
+
+@app.route('/rock/scissors')
+def rock_win():
+    return render_template('rock.html', title="Rock Wins"), )
+
+@app.route('scissors/paper')
+def scissors_win():
+    return render_template('scissors.html', title='Scissors Wins', )
+
+@app.route('paper/rock')
+def paper_win():
+    return render_template('paper.html', title='Paper Wins', )
+
