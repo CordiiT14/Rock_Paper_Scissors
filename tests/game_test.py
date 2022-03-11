d41ac9748cd2f1ea1e6bc509451f6c98ebfddfb5
CordiiT14 <cordelia.toennies@live.com>
2022-03-11 11:33:49 +0000

    Game and Player class set up tested

--- tests/game_test.py
+++ tests/game_test.py
@@ -5,17 +5,18 @@ from models.player import Player
 class TestGame(unittest.TestCase):
 
     def setUp(self):
-        player1 = Player("Robbie", "rock")
-        player2 = Player("Murphy", "scissors")
-        player3 = Player("Charles", "paper")
+        self.player1 = Player("Robbie", "rock")
+        self.player2 = Player("Murphy", "scissors")
+        self.player3 = Player("Charles", "paper")
 
     
     def test_player_attribute_name(self):
         self.assertEqual("Robbie", self.player1.name)
     
     def test_player_attribute_choice(self):
-        self.asserEqual("paper", self.player3.choice)
+        self.assertEqual("paper", self.player3.choice)
 
-    # def test_add_players_to_game(self):
-    #     game = Game(self.player1, self.player2)
-    #     self.assertEqual(2, len(game))
+    def test_add_players_to_game(self):
+        game = Game(self.player1, self.player2)
+        self.assertEqual(2, len(game.players))
+    
