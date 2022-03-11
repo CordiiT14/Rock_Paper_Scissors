7eb22046ac3482de6304c99dd2c7c5d741610838
CordiiT14 <cordelia.toennies@live.com>
2022-03-11 16:43:33 +0000

    controller takes form and spits out winner results

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import redirect, render_template, request, redirect
 from app import app
 from models.player import Player
 from models.game import Game
@@ -19,3 +19,18 @@ def scissors_win():
 def paper_win():
     return render_template('paper.html', title='Paper Wins', )
 
+@app.route('/play', methods=['POST'])
+def display_winner():
+    # if request.method == 'POST':
+        player_1_name = request.form['player1_name']
+        player_1_choice = request.form['p1_choice']
+        player_2_name = request.form['player2_name']
+        player_2_choice = request.form['p2_choice']
+        player1 = Player(player_1_name, player_1_choice)
+        player2 = Player(player_2_name, player_2_choice)
+        game = Game()
+        game.add_players(player1)
+        game.add_players(player2)
+        winner = game.return_winner(game)
+        # return redirect(f'/<player_1_choice>/<player_2_choice>')
+        return render_template('index.html', title='The Winner is...', winner=winner )
